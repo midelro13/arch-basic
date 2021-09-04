a36dea4f624be5f85a66f8a1cd0287da1175095d
midelrocks <72671950+midelrocks@users.noreply.github.com>
2021-09-04 16:55:33 +0200

    Update cinnamon.sh

--- cinnamon.sh
+++ cinnamon.sh
@@ -3,7 +3,8 @@
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-sudo reflector -c Switzerland -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --country Spain -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Sy
 
 sudo firewall-cmd --add-port=1025-65535/tcp --permanent
 sudo firewall-cmd --add-port=1025-65535/udp --permanent
@@ -24,12 +25,6 @@ pikaur -S --noconfirm pix
 #pikaur -S --noconfirm auto-cpufreq
 #sudo systemctl enable --now auto-cpufreq
 
-sudo pacman -S --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings cinnamon firefox simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc xed xreader metacity gnome-shell
-
-sudo flatpak install -y spotify
-sudo flatpak install -y kdenlive
+sudo pacman -S xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings cinnamon firefox simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc xed xreader metacity gnome-shell
 
 sudo systemctl enable lightdm
-/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
-sleep 5
-sudo reboot
