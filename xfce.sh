46864d052100a1e5b7548d716371bee8cb1a7a0f
midelrocks <72671950+midelrocks@users.noreply.github.com>
2021-09-04 16:52:17 +0200

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -4,6 +4,7 @@ sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
 sudo reflector --country Spain -a 2 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Sy
 
 sudo firewall-cmd --add-port=1025-65535/tcp --permanent
 sudo firewall-cmd --add-port=1025-65535/udp --permanent
